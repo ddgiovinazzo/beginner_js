0fbc330d1523e7984b5ad1d45b27e3b703d8149f
Domenico Daniel Giovinazzo <domenicodanielgiovinazzo@gmail.com>
2021-02-10 17:25:19 -0500

    Create base editor

--- src/index.js
+++ src/index.js
@@ -9,31 +9,63 @@ answer.value = ''
 const editor = CodeMirror.fromTextArea(answer, {
   mode: 'javascript',
   theme: 'paraiso-light',
-  autoCloseBrackets: true})
+  autoCloseBrackets: true
+})
+
+editor.setOption("extraKeys", {
+  "Shift-Enter": () => { }
+});
 
 
-const checker = ()=>{
-  const question = document.getElementById('question-box')
-  const value = `${eval(editor.getValue("\n"))}`
+const cm = document.getElementsByClassName("CodeMirror")[0]
+
+
+const buttonDown = (e) => {
+  if (e.keyCode === 13 && e.shiftKey) {
+    button.classList.add("button-down")
+    checker()
+  }
+}
+
+const buttonUp = (e) => {
+  button.classList.remove("button-down")
+}
+
+const checker = () => {
+
   const output = document.getElementById('output-box')
+  const question = document.getElementById('question-box')
   const button = document.getElementById('button')
+  
+  try {
+    eval(editor.getValue("\n"))
+  }
+  
+  catch (error) {
+    output.innerHTML = error
+    output.classList.remove("correct")
+    output.classList.add("incorrect")
+  }
+  const value = `${eval(editor.getValue("\n"))}`
+  output.innerHTML = value
 
-  output.innerHTML = `Output: ${value}`
-
-  if (value === question.innerHTML){
-    question.classList.add("correct")
-    question.classList.remove("incorrect")
+  if (value === question.innerHTML) {
+    output.classList.add("correct")
+    output.classList.remove("incorrect")
 
     button.classList.add("button-correct")
     button.classList.remove("button-incorrect")
     button.innerHTML = "Correct!"
 
-  }else{
-    question.classList.add("incorrect")
+  } else {
+    output.classList.add("incorrect")
+    button.classList.remove("button-correct")
     button.classList.add("button-incorrect")
     button.innerHTML = "Try Again."
-
   }
+
 }
 
 button.addEventListener('click', checker)
+cm.addEventListener('keydown', buttonDown)
+cm.addEventListener('keyup', buttonUp)
